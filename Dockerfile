10ce07ab90e130a855e907684e7099c81383a9a0
Olushola Ejiade <ejiade.olushola@yahoo.com>
2023-02-12 22:29:34 -0500

    script changes

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ RUN apt-get update && apt-get install -y python3.9
 RUN ln -sf /usr/bin/python3.9 /usr/bin/python
 
 # Install Linux packages for debugging and basic operation
-RUN apt-get install -y curl nano htop wget python3-pip net-tools
+RUN apt-get install -y curl nano htop wget python3-pip net-tools git
 
 # Set workdir
 WORKDIR /app
--- script.py
+++ script.py
@@ -1,16 +1,22 @@
-import os import sys import argparse
-from lorem text import lorem
-name try:
-main
-parser = argparse. ArgumentParser (description="File with lines generator")
-parser. add argument (--lines number'
-args = parser-parse_ angs ()
-required=True, type-int, help="Number of lines to be generated")
-paragraph length = args.lines number
-paragraph = lorem.paragraphs (paragraph length)
-with open(os.path.join( f.write (paragraph)
-'file with_lines.txt'),
-w') as f
-except Exception as exc:
-print (f"There was an expected behavior from user or application side. Error: "(exc}!") sys.exit (1)
-SyS.exit (8)
+import os 
+import sys 
+import argparse
+from lorem_text import lorem
+
+if __name__ == "__main__":
+     try
+        parser = argparse. ArgumentParser (description="File with lines generator")
+        parser.add_argument('--lines_number', '-n', required=True, type-int, help="Number of lines to be generated")
+        args = parser.parse_args()
+        
+        paragraph_length = args.lines_number
+        paragraph = lorem.paragraphs(paragraph_length)
+
+        with open(os.path.join('.', 'file_with_lines.txt'), 'w') as f:
+        f.write (paragraph)
+        
+    except Exception as exc:
+        print(f"There was an expected behavior from or application side. Error: '{exc}'") 
+        sys.exit(1)
+
+    sys.exit(0)
